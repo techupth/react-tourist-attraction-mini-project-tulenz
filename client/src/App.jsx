9f2754a5817b8913a63c767f3cc4909fc0aa8c59
Pong <pongpong34756@gmail.com>
2024-06-12 16:33:33 +0700

    feat: skill checkpoint3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Productlist from "../component/ProductList";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  return (
+    <div className="App">
+      <Productlist />
+    </div>
+  );
 }
 
 export default App;
